127dd670e6d0e5f5de3776fe999e3800d03ad769
Mehdi Benrefad <benrefadmehdi@gmail.com>
2021-04-24 02:00:16 +0000

    mono page [View Controller]

--- Pages/ViewController.swift
+++ Pages/ViewController.swift
@@ -9,9 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var Titre: UILabel!
+    @IBOutlet weak var Description: UILabel!
+    
+    var PageTitle=String() , PageDescription=String()
     override func viewDidLoad() {
         super.viewDidLoad()
+        Titre.text = PageTitle
+        Description.text = PageDescription
         // Do any additional setup after loading the view.
     }
 
